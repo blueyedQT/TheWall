31c42a799b1f4ee2f82d5193a727312d285aa1f8
Katrina Sanford <blueyed_qt@hotmail.com>
2014-11-17 09:06:29 -0800

    Inital commit

---
+++ register.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Register</title>
+</head>
+<body>
+	<h1>
+</body>
+</html>
---
+++ wall.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CodingDojo Wall</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<h1>CodingDojo Wall</h1>
+	<p class="header">Welcome Michael</p>
+	<a href="#">Logout</a>
+	<h2>Post a message</h2>
+	<textarea></textarea>
+	<input class="button" type="submit" name="message" value="Post a message">
+	<div class="message">
+		<h3>Michael Choi - January 23rd, 2013</h3>
+		<p>
+			Confidam sit contumax meo res timerent utrimque parentes. Spectentur notionibus ego imaginandi imaginarer tantummodo hoc sum. Liceat rum juncta notior platea res. Ii bile nolo eram vidi scio to id. Negavi sensum agi falsis sed ignota sua vix. At ei idcirco co at motarum figuras divinae reddere. Obnoxius im animalia ferventi ad de callidum. 
+		</p>
+		<div class="comment">
+			<h4>Katrina Sanford - Feb 1st 2013</h4>
+			<p>
+				Liceat rum juncta notior platea res. Ii bile nolo eram vidi scio to id. Negavi sensum agi falsis sed ignota sua vix.
+			</p>
+		</div>
+		<div class="comment">
+			<h4>Katrina Sanford - Feb 1st 2013</h4>
+			<p>
+				Liceat rum juncta notior platea res. Ii bile nolo eram vidi scio to id. Negavi sensum agi falsis sed ignota sua vix.
+			</p>
+		</div>
+		<div class="comment">
+			<h4>Katrina Sanford - Feb 1st 2013</h4>
+			<p>
+				Liceat rum juncta notior platea res. Ii bile nolo eram vidi scio to id. Negavi sensum agi falsis sed ignota sua vix.
+			</p>
+		</div>
+	</div>
+	<div class="message">
+		<h3>Corey Whitelord - January 10th, 2013</h3>
+		<p>
+			Confidam sit contumax meo res timerent utrimque parentes. Spectentur notionibus ego imaginandi imaginarer tantummodo hoc sum. Liceat rum juncta notior platea res. Ii bile nolo eram vidi scio to id. Negavi sensum agi falsis sed ignota sua vix. At ei idcirco co at motarum figuras divinae reddere. Obnoxius im animalia ferventi ad de callidum. 
+		</p>
+		<div class="comment">
+			<h4>Katrina Sanford - Feb 1st 2013</h4>
+			<p>
+				Liceat rum juncta notior platea res. Ii bile nolo eram vidi scio to id. Negavi sensum agi falsis sed ignota sua vix.
+			</p>
+		</div>
+	</div>
+	<div class="message">
+		<h3>Katrina Sanfpord - January 1st, 2013</h3>
+		<p>
+			Confidam sit contumax meo res timerent utrimque parentes. Spectentur notionibus ego imaginandi imaginarer tantummodo hoc sum. Liceat rum juncta notior platea res. Ii bile nolo eram vidi scio to id. Negavi sensum agi falsis sed ignota sua vix. At ei idcirco co at motarum figuras divinae reddere. Obnoxius im animalia ferventi ad de callidum. 
+		</p>
+	</div>
+</body>
+</html>
